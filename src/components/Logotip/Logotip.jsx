408e1dd86f5f5cc82267c1572b1dfc907457e263
Umidjon Amirqulov <uamirqulov5@gmail.com>
2024-03-25 13:07:20 +0500

    create logotip component

--- src/components/Logotip/Logotip.jsx
+++ src/components/Logotip/Logotip.jsx
@@ -2,6 +2,11 @@ import './Logotip.scss'
 
 export function Logotip() {
   return (
-    <div className='circle'></div>
+    <div className="logo">
+     <div className="circle"></div>
+    <div className="logotip">
+        <span className='clr'>D</span><span className="text-gr">ev</span>.
+    </div>
+</div>
   )
 }
